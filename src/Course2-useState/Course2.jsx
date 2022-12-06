f91822cded49b3eb73e9aec189108071c047de01
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-06 20:16:16 +0200

    useState

--- src/Course2-useState/Course2.jsx
+++ src/Course2-useState/Course2.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Course2() {
-  return <div>Course2</div>;
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const handleIncrease = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  console.log("count", count);
+
+  const handleDecrease = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h2>Input</h2>
+      <input type={"text"} onChange={(e) => setInput(e.target.value)} />
+      {input}
+      <h2>Counter</h2>
+      <p>{count}</p>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  );
 }
 
 export default Course2;
